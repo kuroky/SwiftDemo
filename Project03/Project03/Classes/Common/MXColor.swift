d5b6b22c58f2a1fde2dad3d268cccdbed4917859
kurokyfan <kuro2007cumt@gmail.com>
2018-08-06 17:42:40 +0800

    update

---
+++ Project03/Project03/Classes/Common/MXColor.swift
@@ -0,0 +1,20 @@
+//
+//  MXColor.swift
+//  Project03
+//
+//  Created by kuroky on 2018/8/6.
+//  Copyright © 2018年 Kuroky. All rights reserved.
+//
+
+import Foundation
+import Hue
+
+extension UIColor {
+    /// 16字符串进制转颜色
+    class func colorOfHexString(hexString: String) -> UIColor {
+        if hexString.hasPrefix("#") {
+            return UIColor(hex: hexString)
+        }
+        return UIColor(hex: "#" + hexString)        
+    }
+}
--- Project03/Project03/RootView/RootViewController.swift
+++ Project03/Project03/RootView/RootViewController.swift
@@ -17,37 +17,35 @@ class RootViewController: UITabBarController {
     }
     
     func setupAppearance() {
-        
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.foregroundColor: UIColor.lightGray], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.foregroundColor: UIColor.colorOfHexString(hexString: "68BB1E")], for: .selected)
     }
 
     func setup() {
-        let homeVC = HomepageViewController()
-        let homeNavi = MXNavigationController.init(rootViewController: homeVC);
-        let homeBarItem = UITabBarItem(title: "首页", image: nil, selectedImage: nil)
-        homeNavi.tabBarItem = homeBarItem
+        let messageNavi = MXNavigationController.init(rootViewController: MessageViewController());
+        let messageBarItem = UITabBarItem(title: "消息", image: UIImage(named: "tabbar_mainframe")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "tabbar_mainframeHL")?.withRenderingMode(.alwaysOriginal))
+        messageNavi.tabBarItem = messageBarItem
+        
+        let contactsNavi = MXNavigationController.init(rootViewController: ContactsViewController());
+        let contactBarItem = UITabBarItem(title: "通讯录", image: nil, selectedImage: nil)
+        contactsNavi.tabBarItem = contactBarItem
         
         let discoverVC = DiscoverViewController()
         let discoverNavi = MXNavigationController.init(rootViewController: discoverVC);
-        let discoverBarItem = UITabBarItem(title: "最新", image: nil, selectedImage: nil)
+        let discoverBarItem = UITabBarItem(title: "发现", image: nil, selectedImage: nil)
         discoverNavi.tabBarItem = discoverBarItem
-        
-        let messageVC = MessageViewController()
-        let messageNavi = MXNavigationController.init(rootViewController: messageVC);
-        let messageBarItem = UITabBarItem(title: "关注", image: nil, selectedImage: nil)
-        messageNavi.tabBarItem = messageBarItem
         
         let profileVC = ProfileViewController()
         let profileNavi = MXNavigationController.init(rootViewController: profileVC);
-        let profileBarItem = UITabBarItem(title: "更多", image: nil, selectedImage: nil)
+        let profileBarItem = UITabBarItem(title: "我", image: nil, selectedImage: nil)
         profileNavi.tabBarItem = profileBarItem
         
-        let viewControllers: [UIViewController] = [homeNavi, discoverNavi, messageNavi, profileNavi]
+        let viewControllers: [UIViewController] = [messageNavi, contactsNavi, discoverNavi, profileNavi]
         self.viewControllers = viewControllers
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
